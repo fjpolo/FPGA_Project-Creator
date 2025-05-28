8bd3959dbd5346f160e21df6b2e9854a97e12358
fjpolo <fjpolo@gmail.com>
2025-05-29 01:29:54 +0200

    Update amaranth/run.sh

--- template/amaranth/run.sh
+++ template/amaranth/run.sh
@@ -3,4 +3,4 @@
 # FV
 source ~/oss-cad-suite/environment && python3 template.py gen && sby -f template.sby && deactivate
 # cocoTB
-python3 -m venv cocotb_env && source cocotb_env/bin/activate && pip install cocotb && make && deactivate
+python3 -m venv cocotb_env && source cocotb_env/bin/activate && pip install cocotb && pip install pytest && make && deactivate
